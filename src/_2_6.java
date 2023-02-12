53e933c594eefc2f5b99375c95d20f4d1e9aebf1
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2023-02-12 14:47:26 +0900

    _2_6 1try

--- src/_2_6.java
+++ src/_2_6.java
@@ -1,2 +1,47 @@
+import java.util.*;
+import java.io.*;
 public class _2_6 {
+
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+
+        int num = Integer.parseInt(br.readLine());
+
+        String[] list = new String[num];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i<num; i++){
+            list[i] = st.nextToken();
+        }
+        System.out.println(solution(list));
+    }
+
+    public static String solution(String[] list){
+        String answer = "";
+        boolean isPrime = true;
+
+        for(String i : list){
+            StringBuilder sb = new StringBuilder(i);
+
+            int asdf = Integer.parseInt(sb.reverse().toString());
+
+            if(asdf < 2 ) continue;
+
+            for(int j = 2; j<asdf; j++){
+                if(asdf % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if(isPrime){
+                answer += asdf + " ";
+            }
+            isPrime = true;
+
+        }
+        return answer;
+    }
 }
