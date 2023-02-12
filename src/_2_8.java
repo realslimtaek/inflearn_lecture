b5ce9be9863dc39798b281f41fefc1c5084b9c18
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2023-02-12 16:08:39 +0900

    feat: 50%

--- src/_2_8.java
+++ src/_2_8.java
@@ -1,2 +1,40 @@
+import java.util.*;
+import java.io.*;
 public class _2_8 {
+
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        StringTokenizer sb = new StringTokenizer(br.readLine());
+        int[] list = new int[num];
+
+        for(int i = 0; i<num; i++){
+            list[i] = Integer.parseInt(sb.nextToken());
+        }
+        System.out.println(solution(num,list));
+
+    }
+
+    public static String solution(int num, int[] list){
+//        String answer = "";
+        StringBuilder answer = new StringBuilder();
+        int[] winner = new int[num];
+        for(int i = 0; i<num; i++){
+            int cnt = 1;
+            for(int j=0;j<num;j++){
+                if(list[i] < list[j]){
+                    cnt++;
+                }
+                winner[i] = cnt;
+            }
+        }
+
+        for (int j : winner) {
+            answer.append(j).append(" ");
+        }
+        return answer.toString();
+    }
 }
