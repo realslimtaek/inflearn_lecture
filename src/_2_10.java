29ac5abf74bf1e61f46b0a025e9f20c0960c31eb
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2023-02-15 17:29:19 +0900

    feat _2_10 -> 다중 if문을 사용하였던 방식을 고침

--- src/_2_10.java
+++ src/_2_10.java
@@ -20,13 +20,22 @@ public class _2_10 {
 
     public static int solution(int[][] list, int num){
         int answer = 0;
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
 
         for(int i = 1; i < num+1; i++){
             for(int j = 1; j < num+1; j++){
-                if(list[i][j] > list[i-1][j] && list[i][j] > list[i+1][j] &&
-                list[i][j] > list[i][j-1] && list[i][j] > list[i][j+1] ){
-                    answer++;
+                boolean flag = true;
+                for(int k = 0; k<4; k++){
+                    int nx= i+dx[k];
+                    int ny= j+dy[k];
+                    if(list[nx][ny] >= list[i][j]){
+                        flag = false;
+                        break;
+                    }
                 }
+                if(flag) answer++;
+
             }
         }
 
