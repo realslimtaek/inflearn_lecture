419e8ec31abece2ad519c3ff5e0973e24b1cf53d
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2023-03-08 18:10:12 +0900

    idk

--- src/_7_6.java
+++ src/_7_6.java
@@ -1,6 +1,5 @@
 import java.util.*;
-public class _7_6 {
-
+class _7_6{
     static int[] check;
     static int n;
 
@@ -9,24 +8,25 @@ public class _7_6 {
 
         n = sc.nextInt();
         check = new int[n+1];
-
         recursive(1);
     }
 
-    public static void recursive(int L){
-        if(L == n+1){
-            String tmp = "";
-            for (int i = 0; i<check.length; i++) {
-                if (check[i] == 1)
-                    tmp += i + " ";
+    public static void recursive(int a){
+        String answer = "";
+        if(a == n+1){
+            for(int i = 1; i<n+1; i++){
+                if(check[i] == 1)
+                    answer += i + " ";
             }
-            if(tmp.length() > 0) System.out.println(tmp);
-            return;
+            if(answer.length() > 0)
+                System.out.println(answer);
         }else{
-            check[L] = 1;
-            recursive(L+1);
-            check[L] = 0;
-            recursive(L+1);
+            check[a] = 1;
+            recursive(a+1);
+            check[a] = 0;
+            recursive(a+1);
         }
+
+
     }
 }
